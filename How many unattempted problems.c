9ad6c31f15a2a8ed4df3e255a1309fd5d2d97b73
Pragna Yanduri <109524200+Pragna235@users.noreply.github.com>
2022-09-08 22:06:32 +0530

    Add files via upload
    How Many Unattempted problems.

---
+++ How many unattempted problems.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int  i, x, y, remaining;
+	scanf("%d %d",&x, &y);
+	remaining = x-y;
+	printf("%d",remaining);
+
+
+	return 0;
+}
+
