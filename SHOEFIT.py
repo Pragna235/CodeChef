708317701f03e14215b174f8dbdf8a3c63dd84ad
Pragna Yanduri <109524200+Pragna235@users.noreply.github.com>
2022-10-08 22:31:52 +0530

    Add files via upload

---
+++ SHOEFIT.py
@@ -0,0 +1,9 @@
+# cook your dish here
+for i in range(int(input())):
+    a,b,c=map(int,input().split())
+    if(a==0 and b==0 and c==0):
+        print(0)
+    elif (a==1 and b==1 and c==1):
+        print(0)
+    else:
+        print(1)
