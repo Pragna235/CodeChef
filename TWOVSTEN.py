9d537e1957bf207856d486570e47427149308b17
Pragna Yanduri <109524200+Pragna235@users.noreply.github.com>
2022-10-12 23:07:42 +0530

    Add files via upload
    12th October 2022 @ CodeChef

---
+++ TWOVSTEN.py
@@ -0,0 +1,11 @@
+# cook your dish here
+for i in range(int(input())):
+    n=int(input())
+    count=0
+    if(n%10==0):
+        print(0)
+    elif(n%5==0):
+        print(1)
+    else:
+        print(-1)
+                
