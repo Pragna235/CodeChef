d198f9bbcc06148a2d52171a486211e8f5dfd055
Pragna Yanduri <109524200+Pragna235@users.noreply.github.com>
2023-02-25 10:37:20 +0530

    Add files via upload
    25th February 2023

---
+++ PODIUM.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int t,a,b;
+	scanf("%d",&t);
+	while(t--)
+	{
+	    scanf("%d %d",&a, &b);
+	    printf("%d\n",a+b);
+	    
+	    
+	}
+	return 0;
+}
+
