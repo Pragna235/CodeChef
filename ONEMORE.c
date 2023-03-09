04c3de0dae6ad94a1bfacc569b48eb3b3185c9c7
Pragna Yanduri <109524200+Pragna235@users.noreply.github.com>
2023-03-09 22:38:02 +0530

    Add files via upload
    9th March 2023

---
+++ ONEMORE.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int t,x;
+	scanf("%d",&t);
+	while(t--)
+	{
+	    scanf("%d",&x);
+	    if(x>24)
+	    printf("Yes\n");
+	    else
+	    printf("No\n");
+	    
+	}
+	return 0;
+}
+
