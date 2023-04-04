d542e6806fde0e3931c707dc226a0f0a4f6bf3a1
Pragna Yanduri <109524200+Pragna235@users.noreply.github.com>
2023-04-04 21:23:52 +0530

    Add files via upload
    4th April 2023

---
+++ ATM.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    int a;
+    float b;
+    scanf("%d",&a);
+    scanf("%f",&b);
+    if(a%5==0){
+    float c;
+    c=b-(float)a-0.5;
+    if(c<0) printf("%.2f",b);
+    else
+    printf("%.2f",c);
+    }
+    else{
+        printf("%.2f",b);
+    }
+}
+
--- FLOW013.py
+++ FLOW013.py
@@ -1,9 +1,7 @@
+# cook your dish here
 for i in range(int(input())):
-    a,b,c=input().split()
-    a,b,c=int(a),int(b),int(c)
-    if a+b+c==180:
+    a,b,c=map(int,input().split())
+    if(a+b+c == 180):
         print("YES")
     else:
         print("NO")
-        
-    
