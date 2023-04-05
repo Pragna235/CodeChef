616b3d6a815abaf8a6fb6018e3d70b4cf76daed9
Pragna Yanduri <109524200+Pragna235@users.noreply.github.com>
2023-04-05 21:34:18 +0530

    Add files via upload
    5th April 2023

---
+++ SLEEP.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int t,x;
+	scanf("%d",&t);
+	while(t--){
+	    scanf("%d",&x);
+	    if(x<7)
+	    printf("YES\n");
+	    else
+	    printf("NO\n");
+	}
+	return 0;
+}
+
