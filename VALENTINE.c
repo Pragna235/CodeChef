7d377efa9227813805f0c98fa0cef07912818c78
Pragna Yanduri <109524200+Pragna235@users.noreply.github.com>
2023-04-08 20:17:01 +0530

    Update VALENTINE.c

--- VALENTINE.c
+++ VALENTINE.c
@@ -2,6 +2,12 @@
 
 int main(void) {
 	// your code goes here
+	int i=0,t,x,y;
+	scanf("%d",&t);
+	do{
+	    scanf("%d %d",&x,&y);
+	    printf("%d\n",x/y);
+	    i++;
+	}while(i<t);
 	return 0;
 }
-
