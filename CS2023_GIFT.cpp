cb49ff3b06928843aca4e76d9b80483e09b8556a
Pragna Yanduri <109524200+Pragna235@users.noreply.github.com>
2023-07-07 10:22:19 +0530

    Add files via upload

---
+++ CS2023_GIFT.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int x,n,m;
+	cin>>x>>n>>m;
+	if((x+m)>=n)
+	cout<<"YES"<<endl;
+	else
+	cout<<"NO"<<endl;
+	return 0;
+}
