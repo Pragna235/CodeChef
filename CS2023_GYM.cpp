d628cde92426371b6c7bb360d4ef6ae2ea96fdb9
Pragna Yanduri <109524200+Pragna235@users.noreply.github.com>
2023-07-14 22:58:15 +0530

    Add files via upload

---
+++ CS2023_GYM.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	scanf("%d",&t);
+	while(t--){
+	    int n,m;
+	    scanf("%d %d",&n,&m);
+	    printf("%d\n",(m/2)+1);
+	}
+	return 0;
+}
