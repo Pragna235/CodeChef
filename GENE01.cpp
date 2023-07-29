8b246e3ea2f0c5e61625b958451f46315ec479f5
Pragna Yanduri <109524200+Pragna235@users.noreply.github.com>
2023-07-29 22:48:57 +0530

    Add files via upload

---
+++ GENE01.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	char m,d;
+	cin>>m>>d;
+	if(m=='R' || d=='R')
+	cout<<'R'<<endl;
+	 else if(m=='B' || d=='B')
+	cout<<'B'<<endl;
+	else
+	cout<<'G'<<endl;
+	
+	return 0;
+}
