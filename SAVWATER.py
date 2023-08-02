3e4e4d9f2a93848c15366e3e5bb00e075da8e656
Pragna Yanduri <109524200+Pragna235@users.noreply.github.com>
2023-08-02 19:36:28 +0530

    Add files via upload

--- SAVWATER.py
+++ SAVWATER.py
@@ -1,9 +1,8 @@
 # cook your dish here
-for i in range(int(input())):
+for t in range(int(input())):
     h,x,y,c=map(int,input().split())
-    water_for_each=c/h
-    con=x+(y//2)
-    if(con<=water_for_each):
+    water=x+y//2
+    if(h*water<=c):
         print("YES")
     else:
         print("NO")
