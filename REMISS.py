97b8171d63111f967f61ec8a80f127c9b99541d0
Pragna Yanduri <109524200+Pragna235@users.noreply.github.com>
2023-08-11 17:56:16 +0530

    Add files via upload

--- REMISS.py
+++ REMISS.py
@@ -1,8 +1,4 @@
 # cook your dish here
-for i in range(int(input())):
+for t in range(int(input())):
     a,b=map(int,input().split())
-    if(a>=b):
-        maxi=a
-    else:
-        maxi=b
-    print(maxi,a+b)
+    print(max(a,b),a+b)
